9d149fb2d12b09752338bba09df6ebd7d5b5b662
Lisa A <lastembo@gmail.com>
2019-10-10 20:02:48 -0500

    completed Studio16 without Bonus code at the moment

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+
+            for (let astronaut of json) {
+                let div = document.getElementById("container");
+                div.innerHTML += `
+                    <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hourspace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
+                        </ul>
+                    </div >
+                        <img class="avatar" src="${astronaut.picture}">
+                    </div>
+                `;
+            }
+        })
+    });
+})
